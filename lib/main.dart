e368906bad71c3a3b1c946998c560bb090bf227b
Souliya PPS <souliyappsdev@gmail.com>
2022-02-25 14:28:07 +0700

    A2HS_Stable

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,12 @@
-// @dart=2.9
-import 'dart:async';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_web_a2hs/providers/auth_provider.dart';
-import 'package:flutter_web_a2hs/providers/location_provider.dart';
-import 'package:flutter_web_a2hs/screens/MyHomePage.dart';
 import 'package:flutter_web_a2hs/screens/home_screen.dart';
-import 'package:flutter_web_a2hs/screens/map_screen.dart';
-import 'package:flutter_web_a2hs/screens/welcome_screen.dart';
-import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import "package:universal_html/js.dart" as js;
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:animated_text_kit/animated_text_kit.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp();
-
-  // await Firebase.initializeApp(
-  //   options: FirebaseOptions(
-  //     apiKey: "AIzaSyAxGc789lnPyPn4K9Z76VS768s6DbueaEY",
-  //     authDomain: "grocery-store-5d527.firebaseapp.com",
-  //     projectId: "grocery-store-5d527",
-  //     storageBucket: "grocery-store-5d527.appspot.com",
-  //     messagingSenderId: "113866553527",
-  //     appId: "1:113866553527:web:e0e9477256950f58bd3532",
-  //     measurementId: "G-LYW13M3SKE",
-  //   ),
-  // );
-
+void main() {
   runApp(MyApp());
-
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (_) => AuthProvider(),
-        ),
-        ChangeNotifierProvider(
-          create: (_) => LocationProvider(),
-        ),
-      ],
-      child: MyApp(),
-    ),
-  );
 }
 
 class MyApp extends StatelessWidget {
@@ -49,42 +14,193 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Grocery Store',
+      title: 'OLX Shop',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily: 'BaiHyar',
+        primarySwatch: Colors.orange,
       ),
-      // home: MyHomePage(title: 'Grocery Store'),
-      initialRoute: MyHomePage.id,
-      routes: {
-        MyHomePage.id: (context) => MyHomePage(
-              title: 'Grocery Store',
-            ),
-        HomeScreen.id: (context) => HomeScreen(),
-        WelcomeScreen.id: (context) => WelcomeScreen(),
-        MapScreen.id: (context) => MapScreen(),
-      },
-      // home: FutureBuilder(
-      //   future: _checkFirstSeen(),
-      //   builder: (context, snapshot) {
-      //     if (snapshot.hasData) {
-      //       return snapshot.data != null ? WelcomeScreen() : RegisterScreen();
-      //     }
-      //     return Container();
-      //   },
-      // ),
+      home: MyHomePage(),
     );
   }
+}
 
-  // Future<bool> _checkFirstSeen() async {
-  //   SharedPreferences prefs = await SharedPreferences.getInstance();
-  //   bool _seen = (prefs.getBool('seen') ?? false);
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
 
-  //   if (_seen) {
-  //     return false;
-  //   } else {
-  //     await prefs.setBool('seen', true);
-  //     return true;
-  //   }
-  // }
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    if (kIsWeb) {
+      WidgetsBinding.instance!.addPostFrameCallback((_) async {
+        final _prefs = await SharedPreferences.getInstance();
+        final _isWebDialogShownKey = "is-web-dialog-shown";
+        final _isWebDialogShown = _prefs.getBool(_isWebDialogShownKey) ?? false;
+        if (!_isWebDialogShown) {
+          final bool isDeferredNotNull =
+              js.context.callMethod("isDeferredNotNull") as bool;
+
+          if (isDeferredNotNull) {
+            debugPrint(">>> Add to HomeScreen prompt is ready.");
+            await showAddHomePageDialog(context);
+            _prefs.setBool(_isWebDialogShownKey, true);
+          } else {
+            debugPrint(">>> Add to HomeScreen prompt is not ready yet.");
+          }
+        }
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const colorizeColors = [
+      Color.fromARGB(255, 255, 157, 28),
+      Colors.blue,
+      Colors.yellow,
+      Colors.red,
+    ];
+
+    const colorizeTextStyle = TextStyle(
+      fontSize: 50.0,
+      fontWeight: FontWeight.bold,
+      fontFamily: 'Horizon',
+    );
+    return Scaffold(
+      // appBar: AppBar(
+      //   automaticallyImplyLeading: true,
+      //   centerTitle: true,
+      // ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            InkWell(
+              onTap: () {
+                js.context.callMethod("presentAddToHome");
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute<void>(
+                    builder: (BuildContext context) => const HomeScreen(),
+                  ),
+                );
+              }, // Handle your callback.
+              splashColor: Colors.brown.withOpacity(0.5),
+              child: Ink(
+                height: 100,
+                width: 100,
+                decoration: const BoxDecoration(
+                  // ignore: unnecessary_const
+                  image: const DecorationImage(
+                    image: AssetImage('./assets/screen.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            AnimatedTextKit(
+              animatedTexts: [
+                ColorizeAnimatedText(
+                  'OLX Shopping',
+                  textStyle: colorizeTextStyle,
+                  colors: colorizeColors,
+                ),
+              ],
+            ),
+            const SizedBox(height: 15.0),
+            ElevatedButton.icon(
+              icon: const Icon(
+                Icons.add_to_home_screen,
+                color: Colors.white,
+                size: 30.0,
+              ),
+              label: const Text('Add to Home Screen'),
+              style: ElevatedButton.styleFrom(
+                textStyle: const TextStyle(fontSize: 28),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+              ),
+              onPressed: () {
+                js.context.callMethod("presentAddToHome");
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute<void>(
+                    builder: (BuildContext context) => const HomeScreen(),
+                  ),
+                );
+              },
+            ),
+            const SizedBox(height: 25.0),
+            ElevatedButton.icon(
+              icon: const Icon(
+                Icons.menu_open,
+                color: Colors.white,
+                size: 32,
+              ),
+              label: const Text('Go to App'),
+              style: ElevatedButton.styleFrom(
+                textStyle: const TextStyle(fontSize: 25),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+              ),
+              onPressed: () {
+                // js.context.callMethod("presentAddToHome");
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute<void>(
+                    builder: (BuildContext context) => const HomeScreen(),
+                  ),
+                );
+              },
+            ),
+            const SizedBox(height: 27.0),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Future<bool?> showAddHomePageDialog(BuildContext context) async {
+  return showDialog<bool>(
+    context: context,
+    builder: (context) {
+      return Dialog(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Center(
+                  child: Icon(
+                Icons.add_circle,
+                size: 70,
+                color: Theme.of(context).primaryColor,
+              )),
+              SizedBox(height: 20.0),
+              Text(
+                'Add to Homepage',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                'Want to add this application to home screen?',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                  onPressed: () {
+                    js.context.callMethod("presentAddToHome");
+                    Navigator.pop(context, false);
+                  },
+                  child: Text("Yes"))
+            ],
+          ),
+        ),
+      );
+    },
+  );
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,41 +1,39 @@
 // ignore_for_file: deprecated_member_use
-
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_web_a2hs/providers/auth_provider.dart';
-import 'package:flutter_web_a2hs/screens/welcome_screen.dart';
-import 'package:provider/provider.dart';
+
+import '../main.dart';
 
 class HomeScreen extends StatelessWidget {
   // const HomeScreen({ Key? key }) : super(key: key);
   static const String id = 'home-screen';
 
+  const HomeScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    final auth = Provider.of<AuthProvider>(context);
     return Scaffold(
       body: Center(
           child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
           RaisedButton(
-            child: Text('Sign Out'),
+            child: const Text('Sign Out'),
             onPressed: () {
-              auth.error = '';
-              FirebaseAuth.instance.signOut().then((value) {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => WelcomeScreen(),
-                  ),
-                );
-              });
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute<void>(
+                  builder: (BuildContext context) => MyApp(),
+                ),
+              );
             },
           ),
           RaisedButton(
-            child: Text('Home Screen'),
+            child: const Text('Home Screen'),
             onPressed: () {
-              Navigator.pushNamed(context, WelcomeScreen.id);
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute<void>(
+                  builder: (BuildContext context) => const HomeScreen(),
+                ),
+              );
             },
           ),
         ],
